8544b646838acb76a574a52ad62ec7b2f51a71e2
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2023-03-01 23:57:23 +0100

    pie chart component

---
+++ src/components/PieChart.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Pie } from 'react-chartjs-2';
+import { CustomerData } from '../data';
+
+const PieChart = () => {
+  const data = {
+    labels: CustomerData.map((data) => data.year),
+    datasets: [
+      {
+        label: 'Cutomers Lost',
+        data: CustomerData.map((data) => data.customerLost),
+        backgroundColor: ['#4FD1C5', '#E9D8FD', '#FC8181', '#f3ba2f', '#2a71d0'],
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <h1>Pie Chart</h1>
+      <Pie data={data} />
+    </div>
+  );
+};
+
+export default PieChart;
