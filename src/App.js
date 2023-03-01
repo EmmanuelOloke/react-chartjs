29ed08bfabe1d373ae19761298620533dd1c5b98
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2023-03-01 23:58:13 +0100

    rendered the components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import BarChart from './components/BarChart';
+import LineChart from './components/LineChart';
+import PieChart from './components/PieChart';
 
 function App() {
   return (
-    <div className="App">
-      <>Things goes in here</>
+    <div className="App" style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+      <div style={{ width: '60vw', marginBottom: '2em' }}>
+        <BarChart />
+      </div>
+      <div style={{ width: '60vw', marginBottom: '2em' }}>
+        <LineChart />
+      </div>
+      <div style={{ width: '700px', marginBottom: '2em' }}>
+        <PieChart />
+      </div>
     </div>
   );
 }
