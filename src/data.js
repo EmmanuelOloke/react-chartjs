252407389170a23ec462fdc1c2cae64782250e8c
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2023-03-01 23:56:51 +0100

    file to hold and export the data needed

---
+++ src/data.js
@@ -0,0 +1,95 @@
+export const CustomerData = [
+  {
+    id: 1,
+    year: 2017,
+    customerLost: 820,
+    customerGain: 60000,
+  },
+  {
+    id: 2,
+    year: 2018,
+    customerLost: 435,
+    customerGain: 35000,
+  },
+  {
+    id: 3,
+    year: 2019,
+    customerLost: 565,
+    customerGain: 48000,
+  },
+  {
+    id: 4,
+    year: 2020,
+    customerLost: 230,
+    customerGain: 50000,
+  },
+  {
+    id: 5,
+    year: 2021,
+    customerLost: 120,
+    customerGain: 40000,
+  },
+];
+
+export const BalanceData = [
+  {
+    id: 1,
+    month: 'Jan',
+    balance: 500,
+  },
+  {
+    id: 2,
+    month: 'Feb',
+    balance: 300,
+  },
+  {
+    id: 3,
+    month: 'Mar',
+    balance: 400,
+  },
+  {
+    id: 4,
+    month: 'Apr',
+    balance: 500,
+  },
+  {
+    id: 5,
+    month: 'May',
+    balance: 800,
+  },
+  {
+    id: 6,
+    month: 'Jun',
+    balance: 650,
+  },
+  {
+    id: 7,
+    month: 'Jul',
+    balance: 700,
+  },
+  {
+    id: 8,
+    month: 'Aug',
+    balance: 690,
+  },
+  {
+    id: 9,
+    month: 'Sep',
+    balance: 1000,
+  },
+  {
+    id: 10,
+    month: 'Oct',
+    balance: 1200,
+  },
+  {
+    id: 11,
+    month: 'Nov',
+    balance: 1050,
+  },
+  {
+    id: 12,
+    month: 'Dec',
+    balance: 1300,
+  },
+];
